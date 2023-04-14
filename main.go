0c2ccf1e63a4c6355600dc88e9a8c2cdb404a4ad
KantaNakamura <0706363@gmail.com>
2023-04-14 22:33:47 +0800

    Fetching by ID

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
-	// "errors"
+	"errors"
 )
+
 
 type book struct{
 	ID string `json:"id"`
@@ -13,15 +14,42 @@ type book struct{
 	Quantity int `json:"quantity"`
 }
 
+
 var books = []book{
 	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
 	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
 }
 
+
 func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
+
+
+func bookById(c *gin.Context) {
+	id := c.Param("id")
+	book, err := getBookById(id)
+	
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+	
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+
+func getBookById(id string) (*book, error) {
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+	
+	return nil, errors.New("book not found")
+}
+
 
 func createBook(c *gin.Context) {
 	var newBook book
@@ -38,6 +66,7 @@ func createBook(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.GET("/books/:id", bookById)
 	router.POST("/books", createBook)
 	router.Run("localhost:8000")
 }
