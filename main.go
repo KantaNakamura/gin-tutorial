d45d3cb41ad0ad865501ec1e85b22ab5381e2a75
KantaNakamura <0706363@gmail.com>
2023-04-17 20:24:54 +0800

    patch requests

--- main.go
+++ main.go
@@ -40,6 +40,31 @@ func bookById(c *gin.Context) {
 }
 
 
+func checkoutBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+	
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missig id query parameter."})
+		return
+	}
+	
+	book, err := getBookById(id)
+	
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return 
+	}
+	
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available"})
+		return 
+	}
+
+	book.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+
 func getBookById(id string) (*book, error) {
 	for i, b := range books {
 		if b.ID == id {
@@ -68,5 +93,6 @@ func main() {
 	router.GET("/books", getBooks)
 	router.GET("/books/:id", bookById)
 	router.POST("/books", createBook)
+	router.PATCH("/checkout", checkoutBook)
 	router.Run("localhost:8000")
 }
